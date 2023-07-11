bbc04e759934fed56c804a7808736a17fc41e931
Muhammad Daanyal <daanyalhere@gmail.com>
2023-07-11 11:27:31 +0500

    CRUD Operations for User Entity in app.ts

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,43 @@
 import "reflect-metadata"
 import express from "express"
 import { DataSource } from "typeorm";
+import { User } from "./entities/User";
 
 const app = express();
-app.use('/', function(req, res) {
-    res.send('Hello From Express!');
+app.use('/', async function(req, res) {
+    // Workflow of TypeORM: It gets the entity and and make its repository and converts it to SQL, then with the help of Database Driver (in this case: pg) forms the tables of those found entities in the database
+
+    // Now getting that repository
+    const userRepository = AppDataSource.getRepository(User);
+
+    // using that repository, now find all records in that repository/User table/entity in the database
+
+    // Get All the records
+    // const allRecords = await userRepository.find(); // if we don't privide anything in the parameter of find(), it'll result all the rows in the table; but if we provide some id, it will return only that row as per id
+    // res.json(allRecords);
+
+    // Get a single record
+    // const record = await userRepository.findOne({ where: { firstName: "Daanyal" }});
+    // res.json(record);
+
+    // Delete
+    // await userRepository.delete(2); // deleted the record with id = 2;
+    // res.send("Record deleted");
+
+    // Insert
+    // let user: User = new User();
+    // user.firstName = "John";
+    // user.lastName = "Doe";
+    // user.email = "john@gmail.com";
+
+    // const userInserted = await userRepository.save(user);
+    // res.json(userInserted);
+
+    // Update
+    // await userRepository.update(12, { firstName: "John Updated", lastName: "Doe Updated", email: "updated@gmail.com" });
+    // res.send("Record updated");
+
+    // res.send('Hello From Express!');
 });
 
 const port = 3000;
